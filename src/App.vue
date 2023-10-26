2ab4c716b1bcd32ddce421806ec2c1387818e07c
Guilherme Rocha <devguilhermerocha@gmail.com>
2023-10-25 22:40:59 -0300

    first commit 👨‍🚀

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar/>
+    </div>
+    <div class="column is-three-quarter">
+      <TheForm/>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+import SideBar from './components/SideBar.vue'
+import TheForm from './components/TheForm.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    SideBar,
+    TheForm
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="Stopwatch">
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent ({
+  name: 'SideBar'
+})
+</script>
+
+<style scoped>
+header{
+  background:#0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+h1{
+  padding-top:0.5rem;
+  text-align:center;
+}
+img{
+    max-width:250px;
+  };
+@media only screen and (max-width: 768px){
+  header{
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
