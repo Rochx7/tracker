48b1858a035e6dfbc3894e241d5c120397fa4f83
Guilherme Rocha <devguilhermerocha@gmail.com>
2023-11-30 19:51:28 -0300

    composition API

--- src/views/Projects/Form.vue
+++ src/views/Projects/Form.vue
@@ -1,65 +1,76 @@
 <template>
-  <section >
+  <section>
     <form @submit.prevent="save">
       <div class="field">
-        <label for="project-name" class="label">
-          Project Name
-        </label>
-        <input type="text" class="input" v-model="projectName" id="project-name"/>
+        <label for="project-name" class="label"> Project Name </label>
+        <input
+          type="text"
+          class="input"
+          v-model="projectName"
+          id="project-name"
+        />
       </div>
       <div class="field">
-        <button class="button" type="submit" :disabled="!projectName">Save</button>
+        <button class="button" type="submit" :disabled="!projectName">
+          Save
+        </button>
       </div>
     </form>
   </section>
 </template>
 
 <script lang="ts">
-import {useStore} from '@/store'
-import { defineComponent } from 'vue';
-import {TypeNotification} from "@/interfaces/INotifications"
-import useNotify from "@/hooks/notify"
-import { REGISTER_PROJECT,EDIT_PROJECT } from '@/store/type-actions';
+import { useStore } from "@/store";
+import { defineComponent, ref } from "vue";
+import { TypeNotification } from "@/interfaces/INotifications";
+import useNotify from "@/hooks/notify";
+import { REGISTER_PROJECT, EDIT_PROJECT } from "@/store/type-actions";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
-  name: 'Form',
-  props:{
+  name: "Form",
+  props: {
     id: {
-      type: String
+      type: String,
+    },
+  },
+  setup(props) {
+    const router = useRouter();
+    const store = useStore();
+    const { notify } = useNotify();
+    const projectName = ref("");
+
+    if (props.id) {
+      const project = store.state.project.projects.find(
+        (project) => project.id == props.id
+      );
+      projectName.value = project?.name || "";
     }
-  },
-  mounted() {
-      if(this.id){
-        const project = this.store.state.project.projects.find((project)=> project.id == this.id)
-        this.projectName = project?.name || ''
+
+    const isSuccess = () => {
+      projectName.value = "";
+      notify(
+        TypeNotification.SUCCESS,
+        "Ready!👌",
+        "Your new project is now available."
+      );
+      router.push("/projects");
+    };
+
+    const save = () => {
+      if (props.id) {
+        store
+          .dispatch(EDIT_PROJECT, { id: props.id, name: projectName.value })
+          .then(() => isSuccess());
+      } else {
+        store.dispatch(REGISTER_PROJECT, projectName.value).then(() => isSuccess());
       }
-  },
-  data() {
-    return{
-      projectName: ""
+    };
+
+    return {
+      projectName,
+      save,
     };
   },
-  setup(){
-    const store = useStore()
-    const { notify } = useNotify()
-    return {
-      store,
-      notify
-    }
-  },
-  methods: {
-    save(){
-      if (this.id) {
-        this.store.dispatch(EDIT_PROJECT,{id: this.id, name:this.projectName}).then(()=> this.isSuccess())
-      } else {
-        this.store.dispatch(REGISTER_PROJECT, this.projectName).then(()=> this.isSuccess())
-      }
-    },
-    isSuccess(){
-      this.projectName = ''
-      this.notify(TypeNotification.SUCCESS,  "Ready!👌", "Your new project is now available.")
-      this.$router.push('/projects')
-    }
-  },
-})
+});
 </script>
